ad47a06b705d9abfc0bf18706afb0dd7f671f275
Lucia Liu <lucia.liu14@gmail.com>
2025-11-25 21:13:21 -0800

    added total giving

--- filter_interface.py
+++ filter_interface.py
@@ -16,6 +16,38 @@ st.sidebar.header("Select Filters")
 categories = df["General Category"].unique()
 selected_categories = st.sidebar.multiselect("Select one or more categories", categories)
 
+# Amount funded filter
+amount_funded_ranges = {
+    "> 10,000,000": (10000000, df['Amount Funded'].max()),
+    "9,000,000 - 10,000,000": (9000000, 9999999),
+    "8,000,000 - 9,000,000": (8000000, 8999999),
+    "7,000,000 - 8,000,000": (7000000, 7999999),
+    "6,000,000 - 7,000,000": (6000000, 6999999),
+    "5,000,000 - 6,000,000": (5000000, 5999999),
+    "4,000,000 - 5,000,000": (4000000, 4999999),
+    "3,000,000 - 4,000,000": (3000000, 3999999),
+    "2,000,000 - 3,000,000": (2000000, 2999999),
+    "1,000,000 - 2,000,000": (1000000, 1999999),
+    "500,000 - 1,000,000": (500000, 999999)
+}
+selected_amount_funded_range = st.sidebar.selectbox("Select amount funded range", list(amount_funded_ranges.keys()))
+min_amount_funded, max_amount_funded = amount_funded_ranges[selected_amount_funded_range]
+
+# Total giving filter
+total_giving_ranges = {
+    "> 8,000,000": (8000000, df['Total Giving'].max()),
+    "7,000,000 - 8,000,000": (7000000, 7999999),
+    "6,000,000 - 7,000,000": (6000000, 6999999),
+    "5,000,000 - 6,000,000": (5000000, 5999999),
+    "4,000,000 - 5,000,000": (4000000, 4999999),
+    "3,000,000 - 4,000,000": (3000000, 3999999),
+    "2,000,000 - 3,000,000": (2000000, 2999999),
+    "1,000,000 - 2,000,000": (1000000, 1999999),
+    "500,000 - 1,000,000": (500000, 999999)
+}
+selected_total_giving_range = st.sidebar.selectbox("Select total giving range", list(total_giving_ranges.keys()))
+min_total_giving, max_total_giving = total_giving_ranges[selected_total_giving_range]
+
 # Grant count filter
 grant_count_ranges = {
     "> 120": (120, df['Grant Count'].max()),
@@ -35,27 +67,11 @@ grant_count_ranges = {
 selected_grant_count_range = st.sidebar.selectbox("Select grant count range", list(grant_count_ranges.keys()))
 min_grant_count, max_grant_count = grant_count_ranges[selected_grant_count_range]
 
-# Amount funded filter
-amount_funded_ranges = {
-    "> 10,000,000": (10000000, df['Amount Funded'].max()),
-    "9,000,000 - 10,000,000": (9000000, 9999999),
-    "8,000,000 - 9,000,000": (8000000, 8999999),
-    "7,000,000 - 8,000,000": (7000000, 7999999),
-    "6,000,000 - 7,000,000": (6000000, 6999999),
-    "5,000,000 - 6,000,000": (5000000, 5999999),
-    "4,000,000 - 5,000,000": (4000000, 4999999),
-    "3,000,000 - 4,000,000": (3000000, 3999999),
-    "2,000,000 - 3,000,000": (2000000, 2999999),
-    "1,000,000 - 2,000,000": (1000000, 1999999),
-    "500,000 - 1,000,000": (500000, 999999)
-}
-selected_amount_funded_range = st.sidebar.selectbox("Select amount funded range", list(amount_funded_ranges.keys()))
-min_amount_funded, max_amount_funded = amount_funded_ranges[selected_amount_funded_range]
-
 filtered_df = df[
     (df["General Category"].isin(selected_categories)) &
     (df["Grant Count"] >= min_grant_count) & (df["Grant Count"] <= max_grant_count) &
-    (df["Amount Funded"] >= min_amount_funded) & (df["Amount Funded"] <= max_amount_funded)]
+    (df["Amount Funded"] >= min_amount_funded) & (df["Amount Funded"] <= max_amount_funded) &
+    (df["Total Giving"] >= min_total_giving) & (df["Total Giving"] <= max_total_giving)]
 
 st.write(f"{len(filtered_df)} grantmakers found")
 st.dataframe(filtered_df)
